e6d0cc4bea6a11096ce8e7f917bc827ff9ee0aa9
vysakh dileep <vysakh dileep>
2016-09-28 15:25:07 +0530

    Commit

---
+++ app/src/main/java/com/example/user/starbuzz/TopLevelActivity.java
@@ -0,0 +1,54 @@
+package com.example.user.starbuzz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class TopLevelActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_level);
+        //Create an OnItemClickListener
+        CharSequence text = "Sorry Item Currently Unavailable!!";
+        int duration = Toast.LENGTH_LONG;
+        final Toast toast = Toast.makeText(this, text, duration);
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> listView,
+                                            View v,
+                                            int position,
+                                            long id) {
+                        if (position == 0) {
+                            Intent intent = new Intent(TopLevelActivity.this,
+                                    DrinkCategoryActivity.class);
+                            startActivity(intent);
+                        }
+
+
+
+
+                        else
+
+
+
+                       {
+
+                        toast.show();
+
+                       }
+
+
+                    }
+
+                };
+        //Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+}
